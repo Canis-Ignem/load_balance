81844a59cd6f8a8929e89233e6bf00987babe13e
Canis Ignem <jonperezetxebarria@gmail.com>
2021-09-06 14:46:30 +0200

    api test

--- app1/app1.py
+++ app1/app1.py
@@ -1,14 +1,18 @@
 from flask import request, Flask
+from flask_restful import Resource, Api, reqparse
 import json
 
 
 app1 = Flask(__name__)
+api = Api(app1)
 
+class Users(Resource):
 
-@app1.route('/')
-def hello_world():
-    return 'Salam alikom, this is App1 :) '
+    def get(self):
+        
+        return "Hello", 200
 
+api.add_resource(Users, '/users')
 
 if __name__ == '__main__':
    app1.run(debug=True, host='0.0.0.0')
--- app2/app2.py
+++ app2/app2.py
@@ -1,13 +1,17 @@
 from flask import request, Flask
+from flask_restful import Resource, Api, reqparse
 import json
 
 app2 = Flask(__name__)
+api = Api(app2)
 
+class Users(Resource):
 
-@app2.route('/')
-def hello_world():
-    return ' Salam alikom, this is App2 :)'
+    def get(self):
+        
+        return "Hello2", 200
 
+api.add_resource(Users, '/users')
 
 if __name__ == '__main__':
    app2.run(debug=True, host='0.0.0.0')
